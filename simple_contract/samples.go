a1c787aa6f998954aac19297c6318d97efa90a76
Rahul Gupta <rahul.gupta@us.ibm.com>
2016-10-17 15:42:28 -0500

    new contract

---
+++ simple_contract/samples.go
@@ -0,0 +1,31 @@
+package main
+
+var samples = `
+{
+	"event": {
+		"assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
+		"weight": 1200.43,
+		"system": {
+			"cpu": 24,
+			"memory": 56
+		},
+		"temperature": 72.3,
+		"speed": 1791,
+		"power": 10.23
+	},
+	"initEvent": {
+		"nickname": "ELEVATOR",
+		"version": "The ID of a managed asset. The resource focal point for a smart contract."
+	},
+	"state": {
+		"assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
+		"weight": 1200.43,
+		"system": {
+			"cpu": 24,
+			"memory": 56
+		},
+		"temperature": 72.3,
+		"speed": 1791,
+		"power": 10.23
+	}
+}`
